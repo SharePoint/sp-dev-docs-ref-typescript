30ad9b7bacda4a039dcea30b680b2b0607a5dca2
Vesa Juvonen <vesa.juvonen@hotmail.com>
2019-08-20 13:22:16 +0300

    Refresh of the SPFx API documentation

--- SP-Framework/overview/sharepoint.md
+++ SP-Framework/overview/sharepoint.md
@@ -1,3 +1,10 @@
+---
+title: SharePoint Framework TypeScript Reference
+description: The SharePoint Framework object model is built in TypeScript. 
+ms.date: 08/20/2019
+ms.prod: sharepoint
+---
+
 # SharePoint Framework Reference
 
 The SharePoint Framework object model is built in TypeScript. Use this section to learn more about the classes, methods, and other types available for building your SharePoint Framework solutions.
--- generate-docs/json/recopy.bat
+++ generate-docs/json/recopy.bat
@@ -1,17 +1,17 @@
 @echo off
-copy /y ..\..\..\sp-client\spfx-core\sp-application-base\dist\*.api.json . 
-copy /y ..\..\..\sp-client\spfx-core\sp-component-base\dist\*.api.json . 
-copy /y ..\..\..\sp-client\spfx-core\sp-core-library\dist\*.api.json . 
-copy /y ..\..\..\sp-client\spfx-core\sp-extension-base\dist\*.api.json . 
-copy /y ..\..\..\sp-client\spfx-core\sp-http\dist\*.api.json . 
-copy /y ..\..\..\sp-client\spfx-core\sp-odata-types\dist\*.api.json . 
-copy /y ..\..\..\sp-client\spfx-core\sp-page-context\dist\*.api.json . 
-copy /y ..\..\..\sp-client\spfx-core\sp-webpart-base\dist\*.api.json . 
-copy /y ..\..\..\sp-client\spfx-core\decorators\dist\*.api.json . 
+copy /y ..\..\..\sp-client\spfx-core\sp-application-base\temp\*.api.json . 
+copy /y ..\..\..\sp-client\spfx-core\sp-component-base\temp\*.api.json . 
+copy /y ..\..\..\sp-client\spfx-core\sp-core-library\temp\*.api.json . 
+copy /y ..\..\..\sp-client\spfx-core\sp-extension-base\temp\*.api.json . 
+copy /y ..\..\..\sp-client\spfx-core\sp-http\temp\*.api.json . 
+copy /y ..\..\..\sp-client\spfx-core\sp-odata-types\temp\*.api.json . 
+copy /y ..\..\..\sp-client\spfx-core\sp-page-context\temp\*.api.json . 
+copy /y ..\..\..\sp-client\spfx-core\sp-webpart-base\temp\*.api.json . 
+copy /y ..\..\..\sp-client\spfx-core\decorators\temp\*.api.json . 
 
-copy /y ..\..\..\sp-client\spfx-core\sp-dialog\dist\*.api.json . 
-copy /y ..\..\..\sp-client\spfx-core\sp-listview-extensibility\dist\*.api.json . 
-copy /y ..\..\..\sp-client\spfx-core\sp-dynamic-data\dist\*.api.json . 
-copy /y ..\..\..\sp-client\spfx-core\sp-diagnostics\dist\*.api.json . 
+copy /y ..\..\..\sp-client\spfx-core\sp-dialog\temp\*.api.json . 
+copy /y ..\..\..\sp-client\spfx-core\sp-listview-extensibility\temp\*.api.json . 
+copy /y ..\..\..\sp-client\spfx-core\sp-dynamic-data\temp\*.api.json . 
+copy /y ..\..\..\sp-client\spfx-core\sp-diagnostics\temp\*.api.json . 
 
 pause
